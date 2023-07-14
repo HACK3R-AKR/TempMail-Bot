f709da88078dea23e2d865de4c58f4d606cde567
HACK3R-AKR <80332174+HACK3R-AKR@users.noreply.github.com>
2023-07-14 12:37:01 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("6348037983:AAGdoqmgOaTyjfQxpv2jvnuC2_DDa8QbniU")
 API_ID = int(os.environ.get("5329569"))
 API_HASH = os.environ.get("adcfac3b2c5ec0b803fdbfa69e7cf272")
 
-# If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the approriate values on it and put the hash symbol before the lines 3,4,5
+# If You're hosting in VPS replace the following lines 9,10,11 hash symbol and replacing the pproriate values on it and put the hash symbol before the lines 3,4,5
 
 # BOT_TOKEN = "<your bottoken>"
 # API_ID = "<your api_id>"
